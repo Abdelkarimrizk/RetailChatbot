40660ac1863221bee53fde0f83cf1cbb84e51ce1
Karim Rizk <karim2003karim@gmail.com>
2025-06-26 22:08:42 -0400

    guess what? even more testing!!

--- react-with-flask/api/app.py
+++ react-with-flask/api/app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 from agent.general import general_bp
 
-app = Flask(__name__, static_folder="../dist", static_url_path="/")
+app = Flask(__name__)
 app.register_blueprint(general_bp)
-
-@app.route("/")
-def index():
-    return app.send_static_file("index.html")
 
 if __name__ == "__main__":
     app.run()
