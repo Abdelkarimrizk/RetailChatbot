6410f115658606c35e66996d36e52ebc48fca7e6
Karim Rizk <karim2003karim@gmail.com>
2025-06-26 22:39:37 -0400

    setting up for deployment

--- react-with-flask/api/app.py
+++ react-with-flask/api/app.py
@@ -9,6 +9,3 @@ app.register_blueprint(general_bp)
 @app.route("/<path:path>")
 def serve_react(path="index.html"):
     return send_from_directory(app.static_folder, path)
-
-if __name__ == "__main__":
-    app.run()
