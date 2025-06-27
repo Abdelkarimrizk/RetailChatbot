717e3d279d7c8222c1f619049010fb40932f5d2e
Karim Rizk <karim2003karim@gmail.com>
2025-06-26 22:36:48 -0400

    I hate render

--- react-with-flask/api/app.py
+++ react-with-flask/api/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from agent.general import general_bp
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../dist", static_url_path="/")
 app.register_blueprint(general_bp)
+
+@app.route("/")
+@app.route("/<path:path>")
+def serve_react(path="index.html"):
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
     app.run()
